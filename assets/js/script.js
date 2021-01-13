2466b2ee9d84c615c82d696b18b431bc518c222b
jrdrenth <john.drenth@gmail.com>
2021-01-12 21:32:43 -0800

    Added hours of the day to the page

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// From class activity 5.1 - 06-SandwichClick
+// $(document).ready() makes sure that the javascript doesn't get run until the HTML is finished loading
+// This is good for cases where the HTML is complex
+$(document).ready(function() {
+    
+});
