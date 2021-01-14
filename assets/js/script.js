7c1185b65380bfa1189cf5875aa893a38ccd0036
jrdrenth <john.drenth@gmail.com>
2021-01-13 23:12:14 -0800

    refactored javascript into smaller functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 $(document).ready(function() {
     
     // Functions
-    function saveTimeBlock() {
+    function saveTimeBlockData() {
         var key = $(this).parent().attr('id');
         var value = $(this).siblings(".description").val(); // could also do: var value = $(this).prev().val();
 
@@ -15,60 +15,78 @@ $(document).ready(function() {
         }
     }
 
+    function getTimeBlockHourById(timeBlockId) {
+        var timeBlockHour;
+
+        if (timeBlockId.includes('PM')) {
+            timeBlockHour = parseInt(timeBlockId.replace('PM', ''));
+            timeBlockHour = timeBlockHour < 12 ? timeBlockHour + 12 : timeBlockHour;
+        } else {
+            timeBlockHour = parseInt(timeBlockId.replace('AM', ''));
+        }
+
+        return timeBlockHour;
+    }
+
+    function clearTimeBlockStatus(timeBlock) {
+
+        // clear the past, present, future designation
+        timeBlock.classList.remove("past");
+        timeBlock.classList.remove("present");
+        timeBlock.classList.remove("future");
+    }
+
+    function setTimeBlockStatus(timeBlock) {
+        // get timeblock hour and current hour
+        let timeBlockId = timeBlock.getAttribute('id');
+        let timeBlockHour = getTimeBlockHourById(timeBlockId);
+        var currentHour = moment().hour() - 10;
+
+        // Clear status before assigning new one
+        clearTimeBlockStatus(timeBlock);
+
+        // evaluate & assign timeblock status as past, present, or future
+        if (timeBlockHour < currentHour) {
+            timeBlock.classList.add("past");
+        } else if (timeBlockHour === currentHour) {
+            timeBlock.classList.add("present");
+        } else {
+            timeBlock.classList.add("future");
+        }
+    }
+
     function updateTimeBlockColors() {
-        var currentHour = moment().hour();
         var timeBlocks = $('#day').children();
 
         // regular javascript for loop
         for (let i = 0; i < timeBlocks.length; i++) {
-            let timeBlock = timeBlocks[i];
-            
-            // clear the past, present, future designation
-            timeBlock.classList.remove("past");
-            timeBlock.classList.remove("present");
-            timeBlock.classList.remove("future");
-            
-            // get timeblock hour
-            let timeBlockId = timeBlock.getAttribute('id');
-            let timeBlockHour;
-            
-            if (timeBlockId.includes('PM')) {
-                timeBlockHour = parseInt(timeBlockId.replace('PM', ''));
-                timeBlockHour = timeBlockHour < 12 ? timeBlockHour + 12 : timeBlockHour;
-            } else {
-                timeBlockHour = parseInt(timeBlockId.replace('AM', ''));
-            }
-            
-            // evaluate & assign timeblock as past, present, or future
-            if (timeBlockHour < currentHour) {
-                timeBlock.classList.add("past");
-            } else if (timeBlockHour === currentHour) {
-                timeBlock.classList.add("present");
-            } else {
-                timeBlock.classList.add("future");
-            }
+            setTimeBlockStatus(timeBlocks[i]);
         }
     }
 
-    function initializeTimeBlocks() {
-        updateTimeBlockColors();
-        
-        // jquery loop
+    function updateTimeBlocksData() {
+
+        // jquery loop thru each textarea element to assign values from localstorage
         $('.description').each(function() {
             var key = $(this).parent().attr('id');
             $(this).val(localStorage.getItem(key));
         });
     }
 
-    // Event Listener for clicking the Save Button
-    $('.saveBtn').click(saveTimeBlock);
+    function initializeTimeBlocks() {
+        updateTimeBlockColors();
+        updateTimeBlocksData();
+    }
 
-    initializeTimeBlocks();
+    // Event Listener for clicking the Save Button
+    $('.saveBtn').click(saveTimeBlockData);
 
     // MOMENT.JS FORMATTING OPTIONS
     // moment().format();                                // "2019-08-12T17:52:17-05:00" (ISO 8601, no fractional seconds)
     // moment().format("dddd, MMMM Do YYYY, h:mm:ss a"); // "Monday, August 12th 2019, 5:52:00 pm"
     // moment().format("ddd, hA");                       // "Mon, 5PM"
     $('#currentDay').text(moment().format("dddd, MMMM Do, YYYY"));
+
+    initializeTimeBlocks();
 
 });
